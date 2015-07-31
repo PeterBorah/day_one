22e4d4fd7e7ce0a271f08d34a0920d5bbe9d548a
Peter <peterborah@gmail.com>
2015-07-31 17:52:02 -0400

    Write stake-claiming contract.

---
+++ test/terra_nullius.js
@@ -0,0 +1,16 @@
+contract('TerraNullius', function(accounts) {
+  it("lets you stake a claim", function(done) {
+    tn = TerraNullius.at(TerraNullius.deployed_address);
+
+    tn.claim("Kilroy was here.").
+    then(function() { return tn.number_of_claims.call() }).
+    then(function(result) { assert.equal(result, 1) }).
+    then(function() { return tn.claims.call(0) } ).
+    then(function(result) {
+      assert.equal(result[0], accounts[0]);
+      assert.equal(result[1], "Kilroy was here.");
+      assert.isAbove(result[2], 0);
+      done();
+    }).catch(done)
+  });
+});
